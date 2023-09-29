e221030db15845c819eb2192682a5707d7e82fa7
Jimbonk Buchanon <jimbonkgit@gmail.com>
2023-09-29 06:45:51 -0400

    Finish ch. 8 exercises

--- hash_tables.c
+++ hash_tables.c
@@ -5,14 +5,19 @@
 #include "arrays.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int *initHashTable(int);
 void findIntersections();
 void findDuplicate();
-int *initHashTable(int);
+void findMissingLetter();
+void firstNonDuplicateLetter();
 
 int main(int argc, char *argv[]) {
     findIntersections();
     findDuplicate();
+    findMissingLetter();
+    firstNonDuplicateLetter();
     return 0;
 }
 
@@ -24,7 +29,10 @@ int *initHashTable(int length) {
     return table;
 }
 
+int hashCharacter(char chr) { return chr - 97; }
+
 // Exercise 1 - O(N)
+// should be [2, 4]
 void findIntersections() {
     int arr1[] = {1, 2, 3, 4, 5};
     int arr2[] = {0, 2, 4, 6, 8};
@@ -56,12 +64,12 @@ void findIntersections() {
 // Exercise 2 - O(N)
 void findDuplicate() {
     const int length = 7;
-    int list[] = {'a', 'b', 'c', 'd', 'c', 'e', 'f'};
+    int list[] = {'a', 'b', 'c', 'd', 'c', 'e', 'f'}; // should be 'c'
     int i, key;
     int *table = initHashTable(26);
 
     for (i = 0; i < length; i++) {
-        key = list[i] - 97;
+        key = hashCharacter(list[i]);
         if (table[key]) {
             printf("Duplicate is '%c'\n", list[i]);
             return;
@@ -70,4 +78,44 @@ void findDuplicate() {
     }
     free(table);
     puts("No duplicate characters found");
+}
+
+// Exercise 3 - O(N)
+void findMissingLetter() {
+    char *string =
+        "the quick brown box jumps over the lazy dog"; // should be 'f'
+    int i, key, length = strlen(string);
+    int *table = initHashTable(length);
+
+    for (i = 0; string[i] != '\0'; i++) {
+        key = hashCharacter(string[i]);
+        table[key] = 1;
+    }
+
+    for (i = 0; i < length; i++) {
+        if (!table[i]) {
+            printf("The missing letter is '%c'\n", i + 97);
+            return;
+        }
+    }
+}
+
+// Exercise 4 - O(N)
+void firstNonDuplicateLetter() {
+    char *string = "minimum"; // should be 'n'
+    int i, key, length = strlen(string);
+    int *table = initHashTable(length);
+
+    for (i = 0; string[i] != '\0'; i++) {
+        key = hashCharacter(string[i]);
+        table[key]++;
+    }
+
+    for (i = 0; string[i] != '\0'; i++) {
+        key = hashCharacter(string[i]);
+        if (table[key] == 1) {
+            printf("The first non-duplicated letter is '%c'\n", string[i]);
+            return;
+        }
+    }
 }
