396c69d66d6f708b364d4acb2d4d07423cd83a30
Jimbonk Buchanon <jimbonkgit@gmail.com>
2023-11-18 07:30:43 -0500

    Fix most of trie word collection

--- tries.c
+++ tries.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define HASH_TABLE_SIZE 26
+#define HASH_TABLE_SIZE 27
 
 typedef struct TrieNode {
     struct TrieNode *table[HASH_TABLE_SIZE];
@@ -15,6 +16,7 @@ TrieNode *newNode() {
 }
 
 int hashCharacter(char chr) { return chr - 97; }
+int unhashCharacter(int index) { return index + 97; }
 
 void insert(TrieNode *trie, const char *word) {
     for (int i = 0; word[i] != '\0'; i++) {
@@ -26,43 +28,80 @@ void insert(TrieNode *trie, const char *word) {
         }
         trie = child;
     }
+    // This marks it as full word
+    trie->table[26] = malloc(sizeof(TrieNode));
 }
 
-int search(TrieNode *trie, const char *word) {
+TrieNode *search(TrieNode *trie, const char *word) {
     for (int i = 0; word[i] != '\0'; i++) {
         char chr = word[i];
         TrieNode *child = trie->table[hashCharacter(chr)];
         if (child == NULL)
-            return 0;
+            return NULL;
         trie = child;
     }
-    return 1;
+    return trie;
+}
+
+struct Words {
+    int index;
+    char **words;
+    int wordSize;
+};
+
+void append(struct Words *words, char *word) {
+    strncpy(words->words[words->index], word, words->wordSize);
+    words->index++;
 }
 
 // depth first
-char **collectAllWords(TrieNode *trie) {
-    char **words = malloc(sizeof(char *) * 20);
-    return words;
+void _collectAllWords(const TrieNode *trie, char *word, struct Words *words) {
+    for (int i = 0; i < HASH_TABLE_SIZE - 1; i++) {
+        TrieNode *child = trie->table[i];
+        if (child != NULL) {
+            char chr = unhashCharacter(i);
+            char *newWord = malloc(words->wordSize);
+            strncpy(newWord, word, words->wordSize);
+            strncat(newWord, &chr, 1);
+            if (child->table[26] != NULL) {
+                append(words, newWord);
+            } else {
+                _collectAllWords(child, newWord, words);
+            }
+        }
+    }
+}
+
+char **collectAllWords(const TrieNode *trie) {
+    const int wordCount = 20;
+    struct Words words;
+    words.index = 0;
+    words.words = malloc(sizeof(char *) * wordCount);
+    words.wordSize = 20;
+    for (int i = 0; i < wordCount; i++)
+        words.words[i] = malloc(words.wordSize);
+    char *word = malloc(words.wordSize);
+
+    _collectAllWords(trie, word, &words);
+    return words.words;
 }
 
 int main(int argc, char *argv[]) {
-    const int length = 3;
-    const char words[][10] = {"ace", "act", "bat"};
+    const int length = 6;
+    const char words[][10] = {"batter", "ace", "ball", "act", "bat", "can"};
     TrieNode *trie = newNode();
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
         insert(trie, words[i]);
-    }
 
-    printf("Has 'bat'? %d\n", search(trie, "bat"));
-    printf("Has 'battery'? %d\n", search(trie, "battery"));
-    printf("Has 'ace'? %d\n", search(trie, "ace"));
+    printf("Has 'bat'? %d\n", search(trie, "bat") == NULL ? 0 : 1);
+    printf("Has 'battery'? %d\n", search(trie, "battery") == NULL ? 0 : 1);
+    printf("Has 'ace'? %d\n", search(trie, "ace") == NULL ? 0 : 1);
 
     puts("All words:");
     char **allWords = collectAllWords(trie);
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 20; i++)
         printf("%s\n", allWords[i]);
-    }
 
     return 0;
 }
